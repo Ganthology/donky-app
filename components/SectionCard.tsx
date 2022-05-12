b85bc414898e47618f880212038bd982e6660882
Ray Gan Boon Kit <gbk1386@gmail.com>
2022-05-12 14:59:38 +0800

    feat: create a section card component

---
+++ components/SectionCard.tsx
@@ -0,0 +1,61 @@
+import { showNotification } from '@mantine/notifications'
+import Image, { StaticImageData } from 'next/image'
+
+export type CardItem = {
+  title: string
+  description?: string
+  imageSrc: string | StaticImageData
+  price: number
+}
+
+interface SectionCardProps {
+  title: string
+  items: CardItem[]
+}
+
+function CardItem({ title, description, imageSrc, price }: CardItem) {
+  return (
+    <div
+      className='flex cursor-pointer'
+      onClick={() =>
+        showNotification({
+          title: 'Item added successfully',
+          message: `${title} added to cart`,
+          autoClose: 1000,
+          color: 'indigo',
+        })
+      }
+    >
+      <div className=' w-20 h-20'>
+        <Image
+          src={imageSrc}
+          alt='food image'
+          className='rounded-lg'
+          placeholder='blur'
+        />
+      </div>
+      <div className='ml-4'>
+        <p className='font-medium'>{title}</p>
+        <p className='text-zinc-400 text-sm'>{description}</p>
+      </div>
+      <p className='font-semibold text-base ml-auto'>${price.toFixed(2)}</p>
+    </div>
+  )
+}
+
+export function SectionCard({ title, items }: SectionCardProps) {
+  return (
+    <div className='bg-zinc-50 px-4 py-4 rounded-md drop-shadow-sm space-y-4'>
+      <h1 className='font-semibold text-lg'>{title}</h1>
+      {items.map(({ title, description, imageSrc, price }) => (
+        <CardItem
+          title={title}
+          description={description}
+          imageSrc={imageSrc}
+          price={price}
+          key={title}
+        />
+      ))}
+    </div>
+  )
+}
